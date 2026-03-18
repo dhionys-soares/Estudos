8c2d2a2f6f1a8a4fea7a67dec2bd7e68b275081e
Dhionys Soares <dhionys.soares@gmail.com>
2026-03-18 09:51:20 -0300

    uptade projects

---
+++ Angular/MyFirstApp/src/app/components/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../../node_modules/@angular/router/types/_router_module-chunk";
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
